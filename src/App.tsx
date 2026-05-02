b29b94d2e67cbdbe80a58d2b76b1e09b5fc8cfec
mextriagung-sketch <mextriagung@gmail.com>
2026-05-02 19:33:55 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -235,7 +235,7 @@ export default function App() {
   };
 
   const generateMessage = (resi: string) => {
-    return encodeURIComponent(`Hallo Kami dari jnt lalan grai aini paket ada telah samapi dengan nomer resi ${resi}`);
+    return encodeURIComponent(`Hallo Kami Dari J&T Lalan Grai Aini Paket Anda Telah Sampai dengan nomer resi ${resi}`);
   };
 
   const openWhatsApp = (contact: Contact) => {
